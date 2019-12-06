9668ab724a1d9ada21126e15d60a29490adeeade
Julia Rizza <contato@juliarizza.com>
2019-12-06 09:29:33 -0300

    make selector extraction via item loaders

--- uol/uol/spiders/uol_2009_spider.py
+++ uol/uol/spiders/uol_2009_spider.py
@@ -1,7 +1,10 @@
 import scrapy
 import datetime
 
-from uol.utils import daterange, beautify
+from scrapy.loader import ItemLoader
+
+from uol.items import UolItem
+from uol.utils import daterange
 
 # parameters definition
 URL = "https://noticias.uol.com.br/arquivohome/{date}home_23.jhtm"
@@ -13,6 +16,7 @@ class UOL2009Spider(scrapy.Spider):
         This version lasts until 2011-10-04.
     """
     name = 'uol_2009'
+
     min_date = datetime.date(2009, 1, 1)
     max_date = datetime.date(2011, 10, 4)
     ignore_containing = [
@@ -23,7 +27,7 @@ class UOL2009Spider(scrapy.Spider):
     ]
 
     def start_requests(self):
-        urls = ["https://noticias.uol.com.br/arquivohome/20111004home_23.jhtm"]
+        urls = ["https://noticias.uol.com.br/arquivohome/20090101home_23.jhtm"]
         # for date in daterange(self.min_date, self.max_date):
         #     date_url = URL.format(date=date.strftime('%Y%m%d'))
         #     urls.append(date_url)
@@ -55,74 +59,56 @@ class UOL2009Spider(scrapy.Spider):
     
     def extract_fsp(self, response):
         CATEGORY_SELECTOR = '.section-masthead h1::text'
-        CATEGORY_LINK_SELECTOR = '.section-masthead h1::text'
-        ARTICLE_SELECTOR = '#news'
-        TITLE_SELECTOR = 'header h1::text'
-        AUTHOR_SELECTOR = 'header .author::text'
-        LOCATION_SELECTOR = ''
-        DATETIME_SELECTOR = 'header time::attr("datetime")'
-        COMMENT_COUNT_SELECTOR = '#article-comments header a.more::text'
-
-        article = response.css(ARTICLE_SELECTOR)
-        title = article.css(TITLE_SELECTOR).get()
-        category = response.css(CATEGORY_SELECTOR).get() or response.css(CATEGORY_LINK_SELECTOR).get()
+        TITLE_SELECTOR = '#news header h1::text'
+        AUTHOR_SELECTOR = '#news header .author::text'
+        LOCATION_SELECTOR = '#news header .author-location::text'
+        DATETIME_SELECTOR = '#news header time::attr("datetime")'
+        COMMENTS_COUNT_SELECTOR = '#article-comments header a.more::text'
         
-        return {
-            "title": beautify(title),
-            "author": beautify(article.css(AUTHOR_SELECTOR).get()),
-            "category": beautify(category),
-            "location": None,
-            "datetime": article.css(DATETIME_SELECTOR).get(),
-            "comment_count": article.css(COMMENT_COUNT_SELECTOR).get(),
-            "url": response.url
-        } if title else None
+        loader = ItemLoader(item=UolItem(), response=response)
+        loader.add_css('title', TITLE_SELECTOR)
+        loader.add_css('author', AUTHOR_SELECTOR)
+        loader.add_css('category', CATEGORY_SELECTOR)
+        loader.add_css('location', LOCATION_SELECTOR)
+        loader.add_css('datetime', DATETIME_SELECTOR)
+        loader.add_css('comments_count', COMMENTS_COUNT_SELECTOR)
+        loader.add_value('url', response.url)
+        return loader.load_item()
 
     def extract_uol(self, response):
         CATEGORY_SELECTOR = '.title-content .container h4.title-name::text'
-        CATEGORY_LINK_SELECTOR = '.title-content .container h4.title-name a::text'
-        ARTICLE_SELECTOR = 'article.article'
-        TITLE_SELECTOR = 'header h1 .h-external::text'
-        CUSTOM_TITLE_SELECTOR = 'header h1 .custom-title::text'
-        AUTHOR_SELECTOR = '.author .p-author::text'
-        LOCATION_SELECTOR = '.author .p-author-local::text'
-        DATETIME_SELECTOR = '.author .p-author.time::attr("ia-date-publish")'
-        COMMENT_COUNT_SELECTOR = '.comments-total h4 strong::text'
+        TITLE_SELECTOR = 'article header h1 .h-external::text, '\
+                         'article header h1 .custom-title::text'
+        AUTHOR_SELECTOR = 'article .author .p-author::text'
+        LOCATION_SELECTOR = 'article .author .p-author-local::text'
+        DATETIME_SELECTOR = 'article .author .p-author.time::attr("ia-date-publish")'
+        COMMENTS_COUNT_SELECTOR = 'article .comments-total h4 strong::text'
 
-        article = response.css(ARTICLE_SELECTOR)
-        title = article.css(TITLE_SELECTOR).get() or article.css(CUSTOM_TITLE_SELECTOR).get()
-        category = response.css(CATEGORY_SELECTOR).get() or response.css(CATEGORY_LINK_SELECTOR).get()
-
-        return {
-            "title": beautify(title),
-            "author": beautify(article.css(AUTHOR_SELECTOR).get()),
-            "category": beautify(category),
-            "location": beautify(article.css(LOCATION_SELECTOR).get()),
-            "datetime": article.css(DATETIME_SELECTOR).get(),
-            "comment_count": article.css(COMMENT_COUNT_SELECTOR).get(),
-            "url": response.url
-        } if title else None
+        loader = ItemLoader(item=UolItem(), response=response)
+        loader.add_css('title', TITLE_SELECTOR)
+        loader.add_css('author', AUTHOR_SELECTOR)
+        loader.add_css('category', CATEGORY_SELECTOR)
+        loader.add_css('location', LOCATION_SELECTOR)
+        loader.add_css('datetime', DATETIME_SELECTOR)
+        loader.add_css('comments_count', COMMENTS_COUNT_SELECTOR)
+        loader.add_value('url', response.url)
+        return loader.load_item()
 
     def extract_blogosfera(self, response):
         CATEGORY_SELECTOR = '.blog-header h1::text'
-        CATEGORY_LINK_SELECTOR = '.blog-header h1 a::text'
-        ARTICLE_SELECTOR = 'article.article'
-        TITLE_SELECTOR = 'header h1 .h-external::text'
-        CUSTOM_TITLE_SELECTOR = 'header h1 .custom-title::text'
-        AUTHOR_SELECTOR = '.author .p-author::text'
-        LOCATION_SELECTOR = '.author .p-author-local::text'
-        DATETIME_SELECTOR = '.author .p-author.time::attr("ia-date-publish")'
-        COMMENT_COUNT_SELECTOR = '.comments-total h4 strong::text'
+        TITLE_SELECTOR = 'article header h1 .h-external::text, '\
+                         'article header h1 .custom-title::text'
+        AUTHOR_SELECTOR = 'article .author .p-author::text'
+        LOCATION_SELECTOR = 'article .author .p-author-local::text'
+        DATETIME_SELECTOR = 'article .author .p-author.time::attr("ia-date-publish")'
+        COMMENTS_COUNT_SELECTOR = 'article .comments-total h4 strong::text'
 
-        article = response.css(ARTICLE_SELECTOR)
-        title = article.css(TITLE_SELECTOR).get() or article.css(CUSTOM_TITLE_SELECTOR).get()
-        category = response.css(CATEGORY_SELECTOR).get() or response.css(CATEGORY_LINK_SELECTOR).get()
-
-        return {
-            "title": beautify(title),
-            "author": beautify(article.css(AUTHOR_SELECTOR).get()),
-            "category": beautify(category),
-            "location": beautify(article.css(LOCATION_SELECTOR).get()),
-            "datetime": article.css(DATETIME_SELECTOR).get(),
-            "comment_count": article.css(COMMENT_COUNT_SELECTOR).get(),
-            "url": response.url
-        } if title else None
+        loader = ItemLoader(item=UolItem(), response=response)
+        loader.add_css('title', TITLE_SELECTOR)
+        loader.add_css('author', AUTHOR_SELECTOR)
+        loader.add_css('category', CATEGORY_SELECTOR)
+        loader.add_css('location', LOCATION_SELECTOR)
+        loader.add_css('datetime', DATETIME_SELECTOR)
+        loader.add_css('comments_count', COMMENTS_COUNT_SELECTOR)
+        loader.add_value('url', response.url)
+        return loader.load_item()
